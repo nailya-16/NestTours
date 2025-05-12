01a67a63e9fd8ef474f5f54a23ccb5929b80002f
Nailya <nailya-16@users.noreply.github.com>
2025-05-13 00:57:26 +0300

    Creating Modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,8 @@
 import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
