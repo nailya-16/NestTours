1810f79a4f36dc06fd2be7159b307f4cf420c9d6
Nailya <nailya-16@users.noreply.github.com>
2025-05-26 14:49:48 +0300

    Creating the Order Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './controllers/users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ToursModule } from './controllers/tours/tours.module';
+import { OrderModule } from './controllers/order/order.module';
 
 @Module({
   imports: [
     UsersModule,
     ToursModule,
+    OrderModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')
   ],
   controllers: [AppController],
